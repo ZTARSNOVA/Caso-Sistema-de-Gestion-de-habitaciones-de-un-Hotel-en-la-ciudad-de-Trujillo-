0875bf32b1d6addf6884c233e4b720a97af0ba82
Camila Zúñiga <91289918+ZTARSNOVA@users.noreply.github.com>
2025-06-03 19:01:04 -0500

    Update Main.java

--- S11_Hotel/S11_Hotel/src/main/java/com/mycompany/s11_hotel/Main.java
+++ S11_Hotel/S11_Hotel/src/main/java/com/mycompany/s11_hotel/Main.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.s11_hotel;
 
-/**
- *
- * @author User
- */
+import com.mycompany.s11_hotel.view.HabitacionView;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new HabitacionView();
+        });
+    }
 }
